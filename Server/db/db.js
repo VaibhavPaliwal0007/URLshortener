0587b1b76deaf35cb6a183cba85d1172c1cdc9ea
VaibhavPaliwal0007 <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2021-08-30 22:58:55 +0530

    slight change in db

--- Server/db/db.js
+++ Server/db/db.js
@@ -6,3 +6,10 @@ mongoose.connect(mongodbUrl, {
     useNewUrlParser : true,
     useUnifiedTopology : true
 })
+
+.then(() => {
+    console.log('Database is connected')
+})
+.catch(error => {
+  console.log(error)
+})
